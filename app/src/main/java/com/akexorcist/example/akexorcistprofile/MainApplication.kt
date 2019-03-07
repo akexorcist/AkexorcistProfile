04630ce15e284fd895d7271671734a7b64f6c5eb
Akexorcist <akexorcist@gmail.com>
2019-03-07 12:52:32 +0700

    Prepare sentry

--- app/src/main/java/com/akexorcist/example/akexorcistprofile/MainApplication.kt
+++ app/src/main/java/com/akexorcist/example/akexorcistprofile/MainApplication.kt
@@ -2,10 +2,13 @@ package com.akexorcist.example.akexorcistprofile
 
 import com.google.android.play.core.splitcompat.SplitCompatApplication
 import com.jakewharton.threetenabp.AndroidThreeTen
+import io.sentry.Sentry
+import io.sentry.android.AndroidSentryClientFactory
 
 class MainApplication : SplitCompatApplication() {
     override fun onCreate() {
         super.onCreate()
         AndroidThreeTen.init(this)
+        Sentry.init(AndroidSentryClientFactory(applicationContext))
     }
 }
