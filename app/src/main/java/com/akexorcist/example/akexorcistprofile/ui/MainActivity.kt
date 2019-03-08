ac9e249d7079fcafba719f9b9e55025c494e5626
Akexorcist <akexorcist@gmail.com>
2019-03-08 14:48:21 +0700

    Optimize dynamic module configuration

--- app/src/main/java/com/akexorcist/example/akexorcistprofile/ui/MainActivity.kt
+++ app/src/main/java/com/akexorcist/example/akexorcistprofile/ui/MainActivity.kt
@@ -1,17 +1,18 @@
 package com.akexorcist.example.akexorcistprofile.ui
 
 import android.content.Intent
+import android.content.IntentSender
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.akexorcist.example.akexorcistprofile.R
-import com.google.android.play.core.splitinstall.SplitInstallException
-import com.google.android.play.core.splitinstall.SplitInstallManager
-import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
-import com.google.android.play.core.splitinstall.SplitInstallRequest
+import com.google.android.play.core.splitinstall.*
+import com.google.android.play.core.splitinstall.model.SplitInstallSessionStatus
 import kotlinx.android.synthetic.main.activity_main.main_button_menu_blogger as buttonBlogger
 import kotlinx.android.synthetic.main.activity_main.main_button_menu_github as buttonGithub
 import kotlinx.android.synthetic.main.activity_main.main_button_menu_stackoverflow as buttonStackOverflow
+
 
 class MainActivity : AppCompatActivity() {
     companion object {
@@ -20,23 +21,21 @@ class MainActivity : AppCompatActivity() {
         const val FEATURE_STACKOVERFLOW = "feature_stackoverflow"
     }
 
+    private val moduleState: MutableMap<String, Int> = mutableMapOf()
+
     private val manager: SplitInstallManager by lazy<SplitInstallManager> {
         SplitInstallManagerFactory.create(applicationContext)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(com.akexorcist.example.akexorcistprofile.R.layout.activity_main)
 
         buttonGithub.setOnClickListener {
             requestDynamicFeature(
                 FEATURE_GITHUB,
                 onSuccess = {
-                    onModuleInstallationSuccess(getString(R.string.github))
                     openActivity("com.akexorcist.example.feature_github.ui.github.GithubActivity")
-                },
-                onError = { exception ->
-                    onModuleInstallationFailure(getString(R.string.github), exception)
                 })
         }
 
@@ -44,48 +43,117 @@ class MainActivity : AppCompatActivity() {
             requestDynamicFeature(
                 FEATURE_BLOGGER,
                 onSuccess = {
-                    onModuleInstallationSuccess(getString(R.string.blogger))
                     openActivity("com.akexorcist.example.feature_blogger.ui.blogger.BloggerActivity")
-                },
-                onError = { exception ->
-                    onModuleInstallationFailure(getString(R.string.blogger), exception)
                 })
         }
 
         buttonStackOverflow.setOnClickListener {
             requestDynamicFeature(
-                FEATURE_BLOGGER,
+                FEATURE_STACKOVERFLOW,
                 onSuccess = {
-                    onModuleInstallationSuccess(getString(R.string.stackoverflow))
                     openActivity("com.akexorcist.example.feature_stackoverflow.ui.stackoverflow.StackOverflowActivity")
-                },
-                onError = { exception ->
-                    onModuleInstallationFailure(getString(R.string.stackoverflow), exception)
                 })
+        }
+
+        manager.registerListener(listener)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        manager.unregisterListener(listener)
+    }
+
+    private val listener = SplitInstallStateUpdatedListener { state: SplitInstallSessionState ->
+        when (state.status()) {
+            SplitInstallSessionStatus.REQUIRES_USER_CONFIRMATION -> {
+                try {
+                    startIntentSender(state.resolutionIntent().intentSender, null, 0, 0, 0)
+                } catch (e: IntentSender.SendIntentException) {
+                    e.printStackTrace()
+                }
+            }
+            SplitInstallSessionStatus.INSTALLED -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    SplitInstallHelper.updateAppInfo(applicationContext)
+                }
+                val modules = state.moduleNames().joinToString(transform = { getFeatureNameByModuleName(it) })
+                onModuleInstallationSuccess(modules)
+                state.moduleNames().map { module ->
+                    moduleState.remove(module)
+                }
+            }
+            SplitInstallSessionStatus.FAILED -> {
+                val modules = state.moduleNames().joinToString(transform = { getFeatureNameByModuleName(it) })
+                onModuleInstallationFailure(modules, state.errorCode())
+                state.moduleNames().map { module ->
+                    moduleState.remove(module)
+                }
+            }
+            SplitInstallSessionStatus.DOWNLOADING -> {
+                state.moduleNames().map { module ->
+                    moduleState[module] = SplitInstallSessionStatus.DOWNLOADING
+                }
+            }
+            SplitInstallSessionStatus.INSTALLING -> {
+                state.moduleNames().map { module ->
+                    moduleState[module] = SplitInstallSessionStatus.INSTALLING
+                }
+            }
+            SplitInstallSessionStatus.PENDING -> {
+                state.moduleNames().map { module ->
+                    moduleState[module] = SplitInstallSessionStatus.PENDING
+                }
+            }
         }
     }
 
     private fun openActivity(className: String) {
-        val intent = Intent()
-        intent.setClassName(packageName, className)
-        startActivity(intent)
+        val intent = Intent().apply {
+            setClassName(packageName, className)
+        }
+        if (packageManager.resolveActivity(intent, 0) != null) {
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, R.string.module_installing, Toast.LENGTH_SHORT).show()
+        }
     }
 
-    private fun requestDynamicFeature(module: String, onSuccess: () -> Unit, onError: (Exception) -> Unit) {
+    private fun requestDynamicFeature(module: String, onSuccess: () -> Unit = {}, onError: (Exception) -> Unit = {}) {
         val request = SplitInstallRequest.newBuilder().apply {
             addModule(module)
         }.build()
-        manager.startInstall(request)
-            .addOnSuccessListener { _ -> onSuccess() }
-            .addOnFailureListener { exception -> onError(exception) }
+        if (!manager.installedModules.contains(module)) {
+            manager.startInstall(request)
+                .addOnSuccessListener { onSuccess() }
+                .addOnFailureListener { exception -> onError(exception) }
+        } else if (moduleState.contains(module)) {
+            Toast.makeText(this, R.string.module_installing, Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun onModuleInstallationSuccess(module: String) {
-        Toast.makeText(this, getString(R.string.dynamic_module_success, module), Toast.LENGTH_SHORT).show()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            SplitInstallHelper.updateAppInfo(applicationContext)
+        }
+        Toast.makeText(
+            this,
+            getString(R.string.dynamic_module_success, module),
+            Toast.LENGTH_SHORT
+        ).show()
     }
 
-    private fun onModuleInstallationFailure(module: String, exception: Exception) {
-        Toast.makeText(this, getString(R.string.dynamic_module_failure, module, exception.message), Toast.LENGTH_SHORT)
-            .show()
+    private fun onModuleInstallationFailure(module: String, errorCode: Int) {
+        Toast.makeText(
+            this,
+            getString(R.string.dynamic_module_failure, module, errorCode),
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    private fun getFeatureNameByModuleName(feature: String): String = when (feature) {
+        FEATURE_GITHUB -> getString(R.string.github)
+        FEATURE_BLOGGER -> getString(R.string.blogger)
+        FEATURE_STACKOVERFLOW -> getString(R.string.stackoverflow)
+        else -> feature
     }
 }
